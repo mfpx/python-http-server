20c461b9d128efe1cc65a8caa83bf64f0a95745a
David <david.stumbra@gmail.com>
2021-06-21 19:12:12 +0100

    Changed stuff

--- server.py
+++ server.py
@@ -2,28 +2,61 @@
 # -Implement multi-port listeners for HTTP/HTTPS
 # -Fix a crash where the certificate is untrusted
 # -Implement better SSL-related crash handling
+# -Change config from JSON to YAML to handle comments
+# -Add threading to support multiple clients
 
 import socket, sys, datetime, json, os, ssl, getopt
 from os import path
 
 content_array = []
+CUSTOM_CONFIG = False
 #blacklist_array = []
+
+# Returns an argument list array
+def getopts(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hi:p:c:",["host=","port=","custom-config="])
+        return opts
+    except getopt.GetoptError:
+        print("server.py -h <hostname> -p <port> -c <path_to_config_file>")
+        sys.exit(1)
+
+# Parses arguments from the array
+for opt, arg in getopts(sys.argv[1:]):
+    if opt == '-h':
+        print("server.py -h <hostname> -p <port> -c <path_to_config_file>") # Print syntax if -h was given
+        sys.exit() # Kill the server after printing help
+    elif opt in ("-i", "--host"):
+        HOST = str(arg)
+    elif opt in ("-p", "--port"):
+        PORT = int(arg)
+    elif opt in ("-c", "--custom-config"):
+        CUSTOM_CONFIG = arg
 
 # Reads the configuration file into an array
 def readcfg():
-    try:
-        global content_array
-        if not content_array: # Is the configuration file loaded into memory?
-            with open('conf.json') as f:
-                    content_array = json.load(f)
-                    return content_array
-        else: # If it is, return the array instead of reading the config file again (reduces iops)
-            return content_array
-    except:
-        msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Unable to read configuration file!'))
-        print(msg)
-        logwrite(msg)
-        sys.exit(0) # Kill the server if unable to find the config file (and no parameters were provided)
+    global content_array
+    if not content_array: # Is the configuration file loaded into memory?
+        if CUSTOM_CONFIG:
+            try:
+                with open(CUSTOM_CONFIG) as f:
+                        content_array = json.load(f)
+                        return content_array
+            except:
+                msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Unable to find specified config file'))
+                print(msg)
+                os._exit(1) # Kill the server ungracefully - prevents duplicate stdout messages
+        else:
+            try:
+                with open('conf.json') as f:
+                        content_array = json.load(f)
+                        return content_array
+            except:
+                msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Unable to read configuration file!'))
+                print(msg)
+                os._exit(1) # Kill the server ungracefully - prevents duplicate stdout messages
+    else: # If it is, return the array instead of reading the config file again (reduces iops)
+        return content_array
 
 try:
     if readcfg()["logging"] == True:
@@ -57,7 +90,7 @@ def readblacklist():
         #if not blacklist_array: # Is the blacklist loaded into memory?
         with open(readcfg()["blacklist"]) as f:
             for line in f:
-                blacklist_array.append(line)
+                blacklist_array.append(line.rstrip('\n'))
             return blacklist_array
         #else: # Returns the blacklist if in memory
         #    return blacklist_array
@@ -66,25 +99,6 @@ def readblacklist():
         print(msg)
         logwrite(msg)
 
-# Returns an argument list array
-def getopts(argv):
-    try:
-        opts, args = getopt.getopt(argv,"hi:p:",["host=","port="])
-        return opts
-    except getopt.GetoptError:
-        print("server.py -h <hostname> -p <port>")
-        sys.exit(1)
-
-# Parses arguments from the array
-for opt, arg in getopts(sys.argv[1:]):
-    if opt == '-h':
-        print("server.py -h <hostname> -p <port>") # Print syntax if -h was given
-        sys.exit() # Kill the server after printing help
-    elif opt in ("-i", "--host"):
-        HOST = str(arg)
-    elif opt in ("-p", "--port"):
-        PORT = int(arg)
-
 # If hostname has not been defined, read config
 if not 'HOST' in locals():
     HOST = readcfg()["hostname"].rstrip() # Read config file
@@ -92,6 +106,19 @@ if not 'HOST' in locals():
 # If port has not been defined, read config
 if not 'PORT' in locals():
     PORT = int(readcfg()["port"]) # Read config file
+
+def httpResponseLoader(status):
+    try:
+        file = open("http_responses/" + status + '.html', 'rb') # open file , r => read , b => byte format
+        response = file.read() # Read the input stream into response
+        file.close() # Close the file once read
+        return response
+
+    except:
+        msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Page for HTTP ' +str(status), ' not found! Closing connection\n')) # Contains \n to separate requests
+        print(msg)
+        logwrite(msg)
+        return False
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET specifies ipv4
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # SO_REUSEADDR specifies that we are only able to bind to the socket if its not currently in use
@@ -117,41 +144,52 @@ try:
         # Encrypt traffic using a certificate
         if readcfg()["use_encryption"]:
             try:
-                secureSocket = ssl.wrap_socket(
-                                                connection,
-                                                server_side=True,
-                                                certfile=readcfg()["path_to_cert"],
-                                                keyfile=readcfg()["path_to_key"],
-                                                ssl_version=ssl.PROTOCOL_TLS
-                                               );
-            except ssl.SSLError:
+                socket = ssl.wrap_socket(
+                                            connection,
+                                            server_side=True,
+                                            certfile=readcfg()["path_to_cert"],
+                                            keyfile=readcfg()["path_to_key"],
+                                            ssl_version=ssl.PROTOCOL_TLS
+                                        );
+            except ssl.SSLError as e:
                 print("[OpenSSL]: Server has encountered a certificate issue! Shutting down...")
+                print("[Debug]: " + str(e))
                 try:
                     sys.exit(0)
                 except SystemExit:
                     os._exit(0)
+
         else:
-            secureSocket = connection
+            socket = connection
 
         msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Incoming connection from ', address[0] +str(':') +str(address[1])))
         print(msg)
         logwrite(msg)
-        request = secureSocket.recv(1024).decode('utf-8')
+        request = socket.recv(1024).decode('utf-8')
         try:
             readblacklist().index(address[0])
 
             header = 'HTTP/1.1 403 Forbidden\n' # 403 to signify that the client is not allowed to access the resource
             header += 'Server: Python HTTP Server\n' # Server name
-            header += 'Cache-Control: no-store' # Tells the client not to cache the responses
+            header += 'Cache-Control: no-store\n' # Tells the client not to cache the responses
+            header += 'Content-Type: text/html\n\n' # Set MIMEtype to text/html
 
-            blacklist_response = header.encode('utf-8')
-            secureSocket.send(blacklist_response)
-            secureSocket.close() # If in blacklist, close the connection
-            socket_closed = True
+            response = httpResponseLoader(readcfg()["blacklist_rcode"]) # Get the 403 page
+            if response == False:
+                socket_closed = True
+                socket.close()
 
-            msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'IP ', address[0], (' in blacklist. Closing connection!\n')))
-            print(msg)
-            logwrite(msg)
+            if response != False:
+                blacklist_response = header.encode('utf-8')
+                blacklist_response += response
+                socket.send(blacklist_response)
+                socket.close() # If in blacklist, close the connection
+
+                msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'IP ', address[0], (' in blacklist. Closing connection!\n')))
+                print(msg)
+                logwrite(msg)
+
+                socket_closed = True
         except Exception as e:
             socket_closed = False
         string_list = request.split(' ') # Split request from spaces
@@ -220,28 +258,21 @@ try:
                     msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Unable to find requested resource!\n')) # Contains \n to separate requests
                     print(msg)
                     logwrite(msg)
-                header = 'HTTP/1.1 404 Not Found\n\n' # If unable to read the specified file, assume it does not exist and return 404
-                status = '404'
+                header = 'HTTP/1.1 404 Not Found\n' # If unable to read the specified file, assume it does not exist and return 404
+                header += 'Server: Python HTTP Server\n' # Server name
+                header += 'Content-Type: text/html\n\n' # MIMEtype set to html
 
-                try:
-                    file = open("http_responses/" + status + '.html', 'rb') # open file , r => read , b => byte format
-                    response = file.read() # Read the input stream into response
-                    file.close() # Close the file once read
-
-                except:
-                    msg = ''.join(('[' +str(datetime.datetime.now().strftime('%c')) +str(']: '), 'Page for HTTP ' +str(status), ' not found! Closing connection\n')) # Contains \n to separate requests
-                    print(msg)
-                    logwrite(msg)
-
+                response = httpResponseLoader('404')
+                if response == False:
                     socket_closed = True
-                    connection.close()
+                    socket.close()
 
             if socket_closed == False:
                 final_response = header.encode('utf-8')
                 final_response += response
 
-                secureSocket.send(final_response)
-                secureSocket.close()
+                socket.send(final_response)
+                socket.close()
 
 except KeyboardInterrupt:
     print("\nReceived KeyboardInterrupt!")
