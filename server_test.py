784ae230fd281276d2ca5f077c4a2f5bba366351
David <david.stumbra@gmail.com>
2021-10-18 17:59:01 +0100

    +Even more tests!

--- server_test.py
+++ server_test.py
@@ -1,5 +1,6 @@
 import server
 import pytest
+from os import path
 
 
 @pytest.fixture
@@ -16,6 +17,18 @@ def test_config_json_imports(get_config_data):
 
 def test_thread_count_higher_than_zero(get_config_data):
     assert get_config_data["threads"] >= 1
+
+
+def test_http_response_loader_returns_false_on_bad_file():
+    assert server.httpResponseLoader('999') is False
+
+
+def test_http_response_loader_returns_file_pointer():
+    assert type(server.httpResponseLoader('404')) == bytes
+
+
+def test_config_file_exists():
+    assert path.isfile("conf.json") is True
 
 
 def test_config_value_types(get_config_data):
